cbb96b3d7c9c5a2b8128c7eb81bc25c4c0a46f46
Aarit Jayaswal <aaritrj@gmail.com>
2025-06-27 18:19:53 -0700

    Will complete within 3 weeks from today.

---
+++ src/_01_algorithms/_1_prime_or_not/PirmeOrNot.java
@@ -0,0 +1,12 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class PirmeOrNot {
+public static void main(String[] args) {
+	String stringNumber = JOptionPane.showInputDialog("Give me a number, any number!");
+	Integer.parseInt();
+}
+	
+	
+}
