cc8d25346bbf1092b7d71d6e286f3ada24a145e9
Aarit Jayaswal <aaritrj@gmail.com>
2025-07-11 18:28:45 -0700

    Fibonacci requires more time. Steps to complete are in the project.

--- src/_01_algorithms/_1_prime_or_not/PirmeOrNot.java
+++ src/_01_algorithms/_1_prime_or_not/PirmeOrNot.java
@@ -4,8 +4,21 @@ import javax.swing.JOptionPane;
 
 public class PirmeOrNot {
 public static void main(String[] args) {
+	boolean isPrime = true;
 	String stringNumber = JOptionPane.showInputDialog("Give me a number, any number!");
  int input = Integer.parseInt(stringNumber);
+ for(int i = 2; i<input; i++) {
+	 if(input %i == 0) {
+		 JOptionPane.showMessageDialog(null, "This number is not prime");
+		 isPrime = false;
+		 
+		 break;
+	 }
+	 
+ }
+ if(isPrime ==true) {
+	 JOptionPane.showMessageDialog(null,"This number is prime!");
+ }
 }
 	
 	
---
+++ src/_01_algorithms/_2_fibonacci/Fibonacci.java
@@ -0,0 +1,19 @@
+package _01_algorithms._2_fibonacci;
+
+public class Fibonacci {
+public static void main(String[] args) {
+	
+}
+}
+
+
+
+
+
+
+
+/*
+ * Research more of Fibonacci Sequence
+ * Work to find out solution
+ * Take 0, take 1, continuously add two numbers, make variable to store them. 
+*/
