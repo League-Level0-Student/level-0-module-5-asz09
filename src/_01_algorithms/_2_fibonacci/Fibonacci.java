d6ece1fd0968878212d66acf966016fec9b7071c
Aarit Jayaswal <aaritrj@gmail.com>
2025-07-18 18:26:39 -0700

    need to finish next week.

--- src/_01_algorithms/_2_fibonacci/Fibonacci.java
+++ src/_01_algorithms/_2_fibonacci/Fibonacci.java
@@ -1,8 +1,17 @@
 package _01_algorithms._2_fibonacci;
 
+import javax.swing.JOptionPane;
+
 public class Fibonacci {
 public static void main(String[] args) {
+	int number1 = 0;
+	int number2 = 1;
+	int sum = number1+number2;
+	JOptionPane.showMessageDialog(null, sum);
 	
+	for(int i = 0; i<10; i++) {
+		JOptionPane.showMessageDialog(null,i);
+	}
 }
 }
 
