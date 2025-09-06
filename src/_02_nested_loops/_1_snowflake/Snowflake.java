4bd7de24d95bf41592845e06c9050aae7730c22a
Aarit Jayaswal <aaritrj@gmail.com>
2025-09-05 18:21:29 -0700

    VERY GOOD progress.

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -19,19 +19,19 @@ rob.moveTo(300, 300);
 rob.penDown();
 
 		// #4. Set the robot’s speed to 5
-rob.setSpeed(5);
+rob.setSpeed(900);
 
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-for(int i = 0; i<15; i++) {
+for(int i = 0; i<6; i++) {
 	
 	
 	
 	
-}
+
 		
 
 			// #6. Set the pen color to black
-			rob.setPenColor(0,0,0);
+			rob.setPenColor(255,187,0);
 			
 			// #7. move the robot by 50
 			rob.move(50);
@@ -39,25 +39,37 @@ for(int i = 0; i<15; i++) {
 			// #8. turn the robot to the right by 60
 			rob.turn(60);
 			
-			// #12. set the pen color to blue for even numbers of i
-			rob.setPenColor(0,0,255);
+		// #12. set the pen color to blue for even numbers of i
+			
+			if (i % 2 == 0) {
+				rob.setPenColor(0,0,255);
+			}
+			else if (i % 1==0) {
+				for(int j = 0; j<6; j++) {
+				rob.setPenColor(255, 0, 255);
+			}
+}
+
 			
 			// #13. set the pen color to red for all odd numbers of i
 			
 			
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
+			for(int j = 0; j<6; j++) {
+				
 			
 			
 					// #10. turn the robot to the left by 60
-		
+		rob.turn(-60);
 					
 					// #11. move the robot by 50 (play the start button)
-					
+					rob.move(50);
 				
-			
+			}
 			// #14. set the pen color to black
-				
+				rob.setPenColor(255,187,0);
 		
 	}
 
+}
 }
