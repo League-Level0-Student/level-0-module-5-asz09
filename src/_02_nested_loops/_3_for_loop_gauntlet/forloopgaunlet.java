687155a43b9119ec53853a057edbf2117876de83
league <league@localhost>
2025-12-12 18:23:03 -0800

    needs extra help

--- src/_02_nested_loops/_3_for_loop_gauntlet/forloopgaunlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/forloopgaunlet.java
@@ -14,7 +14,16 @@ public class forloopgaunlet {
 			for(int i2 = 0; i2 > 201; i++) {
 				
 			}
+			for(int i3 = 0; i3 > 99; i++) {
+				
+			}
+			for(int i4 = 0; i4 > 100; i++) {
+			}	
 			
-		}
+			for(int i3 = 0; i3 < 501; i++) {
+
 	}
+}
+}
+
 }
