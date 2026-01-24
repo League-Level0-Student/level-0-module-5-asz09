5c8a184f754d6887b277be2a388bdd170f111117
league <league@192.168.1.78>
2026-01-23 18:19:17 -0800

    Abt done with the obedient robot, will get Gavin's help next week
    NO ANDREW!

--- src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -1,5 +1,7 @@
 package _03_method_writing._1_obedient_robot;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
@@ -7,17 +9,22 @@ public class ObedientRobot {
 		Robot rob = new Robot();
 		drawSquare(rob);
 		drawTriangle(rob);
-	}
+		JOptionPane.showInputDialog("Which shape do you want?");
+		String input = null;
+		if(input=="square") {
+			rob.penDown();
+			drawSquare(rob);
+
+		}
+		
+		}
+	
 
 	
-	private static void drawTriangle(Robot rob) {
-		// TODO Auto-generated method stub
-		
-	}
 
+// JOptionPane.showInputDialog("Which shape do you want?");
 
 	static void drawSquare(Robot rob){
-		rob.penDown();
 		rob.setSpeed(30);
 		rob.move(200);
 		rob.turn(90);
@@ -26,20 +33,22 @@ public class ObedientRobot {
 		rob.move(200);
 		rob.turn(90);
 		rob.move(200);
-
-		
-	static void drawTriangle(Robot rob1){
-		rob1.penDown();
-		rob1.setSpeed(40);
-		rob1.move(200);
-		rob1.turn(60);
-		rob1.move(200);
-		rob1.turn(60);
-		
 			
-			
+		
 		}
-	}
+	
+	
+static void drawTriangle(Robot rob) {
+	rob.penDown();
+	rob.setSpeed(30);
+	rob.move(200);
+	rob.turn(60);
+	rob.move(200);
+	rob.turn(120);
+	rob.move(200);
+	rob.turn(120);
+	rob.move(200);
+}
 		
 		
 }
